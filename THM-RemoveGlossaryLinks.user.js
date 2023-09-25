87a1a83e460f45c36e157fc130cb3797ed59a7fd
Thomas <92929646+iscsitarget@users.noreply.github.com>
2023-09-25 10:12:13 +0200

    added observer

--- THM-RemoveGlossaryLinks.user.js
+++ THM-RemoveGlossaryLinks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         THM-RemoveGlossaryLinks
 // @namespace    iscsitarget
-// @version      1.0
+// @version      1.1
 // @description  Remove links with the class "glossary-term" and keep the text inside the <a> tags. The text inside these tags doesn't play well with my note-taking application. This solves it.
 // @author       iscsitarget
 // @match        https://tryhackme.com/*
@@ -11,6 +11,7 @@
 (function() {
     'use strict';
 
+    // Remove glossary links
     function removeGlossaryLinks() {
         var glossaryLinks = document.querySelectorAll('a.glossary-term');
         glossaryLinks.forEach(function(link) {
@@ -20,5 +21,24 @@
             link.parentNode.replaceChild(span, link);
         });
     }
-    window.addEventListener('load', removeGlossaryLinks);
+
+    // Remove when new content is added
+    function handleMutations(mutationsList) {
+        mutationsList.forEach(function(mutation) {
+            if (mutation.addedNodes.length) {
+                mutation.addedNodes.forEach(function(addedNode) {
+                    if (addedNode.nodeType === 1) { // Check if it's an element node
+                        removeGlossaryLinks(addedNode);
+                    }
+                });
+            }
+        });
+    }
+
+    // Start observing changes in the entire document
+    var observer = new MutationObserver(handleMutations);
+    observer.observe(document, { subtree: true, childList: true });
+
+    // Remove glossary links on initial page load
+    removeGlossaryLinks();
 })();
